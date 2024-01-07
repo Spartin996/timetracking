7161684cdf6928a86c9dd1ab05f4629065aba99b
Martin Morris <martinryanmorris@gmail.com>
2024-01-07 17:37:47 +1000

    change menu

--- Nav.php
+++ Nav.php
@@ -7,6 +7,6 @@
   <a href=index.php>Home</a>
   <a href=about.php>About</a>
   <a href=all_categories.php>View Categories</a>
-  <a href=filters.php>View ALL entries</a>
+  <a href=filters.php>View Reports</a>
   <a href=# onclick='newWindow(`entries.php`)'>Manual Entry</a>
 </nav>
