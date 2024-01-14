7651c0edddcaf912d8334a0b785d82bcb19f7a08
Martin Morris <martinryanmorris@gmail.com>
2024-01-14 16:12:31 +1000

    add leading 0's to the current time

--- functions.js
+++ functions.js
@@ -7,6 +7,15 @@ function minutesSince(startTime) {
     let hours = currentDate.getHours();
     let minutes = currentDate.getMinutes();
     let seconds = currentDate.getSeconds();
+    if(seconds < 10) {
+      seconds = "0" + seconds;
+    }
+    if(minutes < 10) {
+      minutes = "0" + minutes;
+    }
+    if(hours < 10) {
+      hours = "0" + hours;
+    }
     return hours + ":" + minutes + ":" + seconds;
     console.log(hours + ":" + minutes + ":" + seconds);
   }
